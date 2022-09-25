600d53c42a6cca739b1d999297c7c6175724d24d
arfan36 <arfanur36@gmail.com>
2022-09-25 23:09:54 +0600

    47_5-6 add some side note

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -5,26 +5,33 @@ const addToDb = id => {
 
     // get the shoppingCart    
     const storedCart = localStorage.getItem('shopping-cart');
+    console.log(storedCart);        //!
     if (storedCart) {
         shoppingCart = JSON.parse(storedCart);
+        console.log(storedCart, shoppingCart);      //!
     }
     else {
         shoppingCart = {};
+        console.log(storedCart, shoppingCart);      //!
     }
 
     // add quantity
-    const quantity = shoppingCart[id];
+    const quantity = shoppingCart[id]; // shoppingCart(object) = {id(property): quantity(value)}
+    console.log(quantity, shoppingCart);      //!
     if (quantity) {
         const newQuantity = quantity + 1;
-        shoppingCart[id] = newQuantity;
+        shoppingCart[id] = newQuantity; // shoppingCart(object) = {id(property): newQuantity(value)}
+
         // const newQuantity = +quantity + 1;
         // localStorage.setItem(id, newQuantity);
     }
     else {
-        shoppingCart[id] = 1;
+        shoppingCart[id] = 1; // shoppingCart(object) = {id(property): quantity(value)}
+
         // localStorage.setItem(id, 1);
     }
     localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+    console.log(quantity, storedCart, shoppingCart);      //!
 };
 
 export { addToDb };
