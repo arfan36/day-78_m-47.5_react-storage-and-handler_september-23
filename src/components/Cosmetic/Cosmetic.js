6f540cfbb6cee2245f9710ba509fa1bface99dea
arfan36 <arfanur36@gmail.com>
2022-09-25 18:07:49 +0600

    47_5-4 How to Create Event handler with parameters

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import './Cosmetic.css';
 
 const Cosmetic = (props) => {
-    console.log(props.cosmetic);
+    // console.log(props.cosmetic);
     const { name, price, id } = props.cosmetic;
+
+    //   const addToCart = (id) => {
+    //         console.log('item added', id);
+    //     }
+
+    const addToCart = (id, name) => {
+        console.log(`Name: ${name}, Id: ${id}`);
+    };
+
+    // const addToCartWithParam = () => addToCart(id);
+
     return (
-        <div style={{border: "2px solid lightblue", margin: "10px"}}>
+        <div className='product'>
             <h2>Buy  this: {name}</h2>
             <p>Only for: ${price}</p>
             <p><small>It has id: {id}</small></p>
+            {/* <button onClick={addToCartWithParam}>Add to cart</button> */}
+            {/* <button onClick={() => addToCart(id)}>Add to Cart</button> */}
+            <button onClick={() => addToCart(id, name)}>Add to Cart</button>
         </div>
     );
 };
