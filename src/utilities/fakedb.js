3b93a2dfa40a83b96092b97991b4acc4ae07c9d8
arfan36 <arfanur36@gmail.com>
2022-09-25 23:12:54 +0600

    47_5-6 remove side note

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,37 +1,26 @@
 // use local Storage to manage cart data
 const addToDb = id => {
-    // let shoppingCart = {};
     let shoppingCart;
 
     // get the shoppingCart    
     const storedCart = localStorage.getItem('shopping-cart');
-    console.log(storedCart);        //!
     if (storedCart) {
         shoppingCart = JSON.parse(storedCart);
-        console.log(storedCart, shoppingCart);      //!
     }
     else {
         shoppingCart = {};
-        console.log(storedCart, shoppingCart);      //!
     }
 
     // add quantity
-    const quantity = shoppingCart[id]; // shoppingCart(object) = {id(property): quantity(value)}
-    console.log(quantity, shoppingCart);      //!
+    const quantity = shoppingCart[id];
     if (quantity) {
         const newQuantity = quantity + 1;
-        shoppingCart[id] = newQuantity; // shoppingCart(object) = {id(property): newQuantity(value)}
-
-        // const newQuantity = +quantity + 1;
-        // localStorage.setItem(id, newQuantity);
+        shoppingCart[id] = newQuantity;
     }
     else {
-        shoppingCart[id] = 1; // shoppingCart(object) = {id(property): quantity(value)}
-
-        // localStorage.setItem(id, 1);
+        shoppingCart[id] = 1;
     }
     localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
-    console.log(quantity, storedCart, shoppingCart);      //!
 };
 
 export { addToDb };
