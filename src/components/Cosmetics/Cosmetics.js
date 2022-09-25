313616c207dc11f808b4c18f182746d966e898b3
arfan36 <arfanur36@gmail.com>
2022-09-25 14:49:59 +0600

    47_5-3 clean

--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import { add } from '../../utilities/Calculate';
-// import add from '../../utilities/Calculate';
 
 const Cosmetics = () => {
-    const first = 55;
-    const second = 66;
-    const total = add(first, second);
     return (
         <div>
             <h1>Welcome to my cosmetics store</h1>
-            <p>Total: {total}</p>
         </div>
     );
 };
