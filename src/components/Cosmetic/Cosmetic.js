62c3bf4b817a50b6a3b091c15ddb5aa718ee3f45
arfan36 <arfanur36@gmail.com>
2022-09-25 19:11:18 +0600

    47_5-5 Intro to Local storage to store data just one item

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { addToDb } from '../../utilities/fakedb';
 import './Cosmetic.css';
 
 const Cosmetic = (props) => {
     // console.log(props.cosmetic);
     const { name, price, id } = props.cosmetic;
 
-    //   const addToCart = (id) => {
-    //         console.log('item added', id);
-    //     }
-
-    const addToCart = (id, name) => {
-        console.log(`Name: ${name}, Id: ${id}`);
+    const addToCart = (id) => {
+        addToDb(id);
     };
+
+    // const addToCart = (id, name) => {
+    //     console.log(`Name: ${name}, Id: ${id}`);
+    // };
 
     // const addToCartWithParam = () => addToCart(id);
 
@@ -21,8 +22,8 @@ const Cosmetic = (props) => {
             <p>Only for: ${price}</p>
             <p><small>It has id: {id}</small></p>
             {/* <button onClick={addToCartWithParam}>Add to cart</button> */}
-            {/* <button onClick={() => addToCart(id)}>Add to Cart</button> */}
-            <button onClick={() => addToCart(id, name)}>Add to Cart</button>
+            {/* <button onClick={() => addToCart(id, name)}>Add to Cart</button> */}
+            <button onClick={() => addToCart(id)}>Add to Cart</button>
         </div>
     );
 };
---
+++ src/utilities/fakedb.js
@@ -0,0 +1,16 @@
+// use local Storage to manage cart data
+const addToDb = id => {
+    const quantity = localStorage.getItem(id);
+    if (quantity) {
+        console.log('already exist');
+        const newQuantity = parseInt(quantity) + 1;
+        // const newQuantity = +quantity + 1;
+        localStorage.setItem(id, newQuantity);
+    }
+    else {
+        console.log('new item');
+        localStorage.setItem(id, 1);
+    }
+};
+
+export { addToDb };
