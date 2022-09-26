4b881e2cbf0179af19b161fe2b57a95ae302def4
arfan36 <arfanur36@gmail.com>
2022-09-26 15:07:18 +0600

    47_5-8 Module Summary and simple intro to array reduce

--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { getTotal } from '../../utilities/Calculate';
 import Cosmetic from '../Cosmetic/Cosmetic';
 
 const Cosmetics = () => {
@@ -10,9 +11,12 @@ const Cosmetics = () => {
             .then(data => setCosmetic(data));
     }, []);
 
+    const total = getTotal(cosmetics);
+
     return (
         <div>
             <h1>Welcome to my cosmetics store</h1>
+            <p>Money needed: ${total}</p>
             {
                 cosmetics.map(cosmetic => <Cosmetic
                     key={cosmetic.id}
